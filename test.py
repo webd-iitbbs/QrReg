f83fa2a113afee044843fa28157c985633589a40
MadhavChoudhary <madhavtummala@icloud.com>
2019-01-16 00:16:31 +0530

    Added SQL Support

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ import shutil
 import json
 import hashlib
 import MySQLdb as db
+import os
 
 HOST = "103.27.87.65"
 PORT = 3306
@@ -10,26 +11,33 @@ USER = "esummiti_reg"
 PASSWORD = "!nPiUFjr)&NE"
 DB = "esummiti_register"
 
-for i in range(1,2):
+for i in range(1,501):
 
-	REG_NO="1801"+str(i);
+	REG_NO="1801"+"{0:0=3d}".format(i)
 	CODE = hashlib.md5(REG_NO.encode('utf-8')).hexdigest()
 
 	BASE="http://ca.e-summit-iitbbs.com/register/register.php?p="
 	URL_DATA = BASE+CODE
 
-	print(URL_DATA)
+	#print(URL_DATA)
 
 	URL = "http://api.qrserver.com/v1/create-qr-code/"
 
 	PARAMS = {
 		'data':URL_DATA,
-		'size':"500x500",
+		'size':"300x300",
 		'color':"0-0-255"
 	}
 
+	import os
+	if not os.path.exists('qrcodes'):
+	    os.makedirs('qrcodes')
+
 	r = requests.get(url = URL, params = PARAMS, allow_redirects=True)
-	open(REG_NO+".png", 'wb').write(r.content)
+	open('qrcodes/'+REG_NO+".png", 'wb').write(r.content)
+
+	sql_query="INSERT INTO `qr_code`(`id`, `normal_value`, `hashed`) VALUES (\'NULL\',\'%s\',\'%s\')" % (REG_NO, CODE)
+	print (sql_query)
 
     # try:
     #     connection = db.Connection(host=HOST, port=PORT,user=USER, passwd=PASSWORD, db=DB)
